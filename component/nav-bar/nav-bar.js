dd9df8de1a8c97c4b39bafcce63444fd8f821f43
deanveb <Annhi1975@gmail.com>
2026-01-17 22:16:24 +0700

    -Removed the missed path error

--- component/nav-bar/nav-bar.js
+++ component/nav-bar/nav-bar.js
@@ -1,7 +1,7 @@
 import { inherit } from "/utils/inheritance.js";
 
 function render() {
-  inherit("/component/nav-bar/nav-bar.html", "/component/nav-bar/nav-bar.css", "nav-bar");
+  inherit("component/nav-bar/nav-bar.html", "component/nav-bar/nav-bar.css", "nav-bar");
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
